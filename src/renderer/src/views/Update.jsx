65188bcedd11a55c4106c014f090defc8f507bfd
codebyalexx <a.legal.contact@gmail.com>
2025-04-08 18:36:47 +0200

    :zap: Improved update view UI

--- src/renderer/src/views/Update.jsx
+++ src/renderer/src/views/Update.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react'
+import keplerIcon from '../assets/icon.png'
+import { ExternalLinkIcon } from 'lucide-react'
 
 export function Update() {
   const [progress, setProgress] = useState(0)
@@ -23,34 +25,49 @@ export function Update() {
         className={`bg-[url('/src/assets/bg6.jpg')] bg-center bg-cover backdrop-blur-3xl w-full h-full`}
       >
         <div className="backdrop-blur-xl h-full w-full bg-black/75 p-8 pt-10">
-          <div className="w-full h-full px-14 py-2 flex items-center justify-center">
-            <div className="space-y-2">
-              <h1 className="text-xl font-bold">
-                {ready ? 'Mise à jour terminée' : 'Mise à jour en cours'}
-              </h1>
-              <p className="text-neutral-500 mb-3">
-                {ready
-                  ? "Cliquez sur le bouton ci-dessous afin de procéder à l'installation."
-                  : 'Le Launcher télécharge la dernière mise à jour afin de vous offrir les dernières fonctionnalités.'}
-              </p>
-              {!ready && (
-                <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
-                  <div
-                    className={`bg-violet-600 h-2.5 rounded-full dark:bg-violet-500`}
-                    style={{
-                      width: `${progress}%`
-                    }}
-                  ></div>
+          <div className="w-full h-full px-14 py-2 flex  items-center justify-center ">
+            <div className="space-y-8">
+              <div className="w-full">
+                <img src={keplerIcon} alt="Kepler icon" className="w-16" />
+              </div>
+              <div className="space-y-2 p-8 bg-black/25">
+                <div className="flex items-center justify-between">
+                  <h1 className="text-xl font-bold">
+                    {ready ? 'Mise à jour terminée' : 'Mise à jour en cours'}
+                  </h1>
+                  <a
+                    className="font-medium underline flex items-center gap-2 cursor-pointer"
+                    href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Notes de patch <ExternalLinkIcon className="w-4 h-4" strokeWidth={3} />
+                  </a>
                 </div>
-              )}
-              {ready && (
-                <button
-                  className="p-4 rounded-lg font-semibold text-white bg-violet-500 hover:bg-violet-500"
-                  onClick={handleInstall}
-                >
-                  Redémarrer et installer
-                </button>
-              )}
+                <p className="text-neutral-500 mb-3">
+                  {ready
+                    ? "Cliquez sur le bouton ci-dessous afin de procéder à l'installation."
+                    : 'Le Launcher télécharge la dernière mise à jour afin de vous offrir les dernières fonctionnalités.'}
+                </p>
+                {!ready && (
+                  <div className="w-full bg-white/15 rounded-full h-2.5 mb-4">
+                    <div
+                      className={`bg-violet-600 h-2.5 rounded-full dark:bg-white`}
+                      style={{
+                        width: `${progress}%`
+                      }}
+                    ></div>
+                  </div>
+                )}
+                {ready && (
+                  <button
+                    className="p-4 rounded-lg font-semibold text-black bg-white hover:bg-neutral-200"
+                    onClick={handleInstall}
+                  >
+                    Redémarrer et installer
+                  </button>
+                )}
+              </div>
             </div>
           </div>
         </div>
